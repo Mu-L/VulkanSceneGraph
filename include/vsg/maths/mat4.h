6185c738b5576065e51231d5740814219e4d74d5
Robert Osfield <robert.osfield@gmail.com>
2026-05-10 16:24:14 +0100

    Four bug fixes to issues found by an AI code audit!

--- include/vsg/maths/mat4.h
+++ include/vsg/maths/mat4.h
@@ -236,9 +236,8 @@ namespace vsg
     t_vec3<T> operator*(const t_vec3<T>& lhs, const t_mat4<T>& rhs)
     {
         T inv = numbers<T>::one() / (lhs[0] * rhs[3][0] + lhs[1] * rhs[3][1] + lhs[2] * rhs[3][2] + rhs[3][3]);
-        return t_vec3<T>(lhs[0] * rhs[0][0] + lhs[1] * rhs[0][1] + lhs[2] * rhs[0][2] + rhs[0][3] * inv,
-                         lhs[0] * rhs[1][0] + lhs[1] * rhs[1][1] + lhs[2] * rhs[1][2] + rhs[1][3] * inv,
-                         lhs[0] * rhs[2][0] + lhs[1] * rhs[2][1] + lhs[2] * rhs[2][2] + rhs[2][3] * inv);
+        return t_vec3<T>((lhs[0] * rhs[0][0] + lhs[1] * rhs[0][1] + lhs[2] * rhs[0][2] + rhs[0][3]) * inv,
+                         (lhs[0] * rhs[1][0] + lhs[1] * rhs[1][1] + lhs[2] * rhs[1][2] + rhs[1][3]) * inv,
+                         (lhs[0] * rhs[2][0] + lhs[1] * rhs[2][1] + lhs[2] * rhs[2][2] + rhs[2][3]) * inv);
     }
-
 } // namespace vsg
